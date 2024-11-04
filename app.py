871744d6b1eaec457f824ad882ff5de1693480ad
yvesivan <qyizborganio@tip.edu.ph>
2024-11-04 21:56:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ import numpy as np
 from PIL import Image, ImageDraw
 import base64
 
-# Load and encode the background image to display it in the CSS
 def get_base64_image(image_path):
     with open(image_path, "rb") as image_file:
         base64_str = base64.b64encode(image_file.read()).decode()
     return base64_str
 
-# Set background and custom styles
 def set_background(image_path):
     bg_img_base64 = get_base64_image(image_path)
     bg_css = f"""
@@ -41,42 +39,39 @@ def set_background(image_path):
     """
     st.markdown(bg_css, unsafe_allow_html=True)
 
-# Set the background image and custom styles
 set_background("Bg4.png")
 
-# Model setup
 model_path = "best.onnx"
 ort_session = ort.InferenceSession(model_path)
 
-# Title with semi-transparent border
 st.markdown('<div class="title-container"><h1>Aedes Mosquito Identifier</h1></div>', unsafe_allow_html=True)
 st.write("Upload an image to make a prediction.")
 
-# File uploader
+class_names = {
+    0: "Aedes Aegypti",
+    1: "Aedes Albopictus",
+}
+
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png", "jfif"])
 
-# Process and display image with predictions
 if uploaded_file is not None:
     uploaded_image = Image.open(uploaded_file)
     st.image(uploaded_image, caption="Uploaded Image.", use_column_width=True)
 
-    # Resize and prepare the image for the model
     resized_image = uploaded_image.resize((640, 640))
     image_input = np.array(resized_image).astype(np.float32) / 255.0
     image_input = np.transpose(image_input, (2, 0, 1))
     image_input = image_input[np.newaxis, :, :, :]
 
-    # Run model inference
     outputs = ort_session.run(None, {ort_session.get_inputs()[0].name: image_input})
     detections = outputs[0]
 
-    # Draw boxes for each detection on the resized image
     draw = ImageDraw.Draw(resized_image)
     for detection in detections[0]:
         x1, y1, x2, y2, confidence, class_id = detection
         if confidence > 0.3:
+            class_name = class_names.get(int(class_id), "Unknown")
             draw.rectangle([x1, y1, x2, y2], outline="green", width=2)
-            draw.text((x1, y1 - 10), f"Class: {int(class_id)}, Conf: {confidence:.2f}", fill="red")
+            draw.text((x1, y1 - 10), f"{class_name}, Conf: {confidence:.2f}", fill="red")
 
-    # Display result image with bounding boxes
     st.image(resized_image, caption="Model Prediction", use_column_width=True)
