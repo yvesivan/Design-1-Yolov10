9a79ad35a47a7fe665388dd7dfdb34a02df56fdc
ChingKyleTIP <153418736+ChingKyleTIP@users.noreply.github.com>
2024-11-24 01:30:14 +0800

    Update main_page.py

--- MainPage/main_page.py
+++ MainPage/main_page.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 import streamlit as st
-import base64
-from .ImageGathered.IG import ig_page  # Updated import for ImageGathered
-from .Location.Loc import loc_page  # Updated import for Location
-from .Uploads.Upload import upload_page  # Updated import for Uploads
+from .ImageGathered.IG import ig_page  # Relative import for ImageGathered
+from .Location.Loc import loc_page  # Relative import for Location
+from .Uploads.Upload import upload_page  # Relative import for Uploads
 
 # Function to get base64 encoding of an image
 def get_base64_image(image_path):
